f15fd50612273a73e4e6fec43c7fc7baeb4424d5
May <40745122+SomewhatMay@users.noreply.github.com>
2023-03-27 20:49:48 -0400

    Version 2.0 - With enums and traceback!

--- DebugSystem.lua
+++ DebugSystem.lua
@@ -3,32 +3,35 @@
 -- August 19, 2022
 
 
-
 local debugHandler = {}
 
-function debugHandler.debug(sourceScript: Script, messageType: string, ...: string)
+-- Create enums
+-- If there is not tracebackMode set, the deafult is `TracebackMode.Enabled`
+local TracebackMode = {
+    Enabled = "debugTracebackEnabled";
+    EnabledOpen = "debugTracebackOpen";
+    Disabled = "debugTracebackDisabled";
+    Code = "debugTracebackCode";
+}
+local MessageType = {
+    Output = "messageTypeOutput";
+    Warning = "messageTypeWarning";
+    Error = "messageTypeError";
+};
+
+debugHandler.TracebackMode = TracebackMode
+debugHandler.MessageType = MessageType
+
+function debugHandler.debug(sourceScript: Script, messageType: string, tracebackMode: TracebackMode, ...: string)
+    tracebackMode = tracebackMode or TracebackMode.Enabled
+
     local args = {...}
-    local gotMessageType = false
     local strings = {}
     
     if typeof(sourceScript) == "Instance" and (sourceScript:IsA("BaseScript") or sourceScript:IsA("ModuleScript")) then
         table.insert(strings, "[" .. sourceScript.Name .. "]:")
     elseif sourceScript ~= nil then
-        table.insert(strings, sourceScript)
-    end
-
-    if messageType == "_warn" then
-        gotMessageType = "warn"
-    elseif messageType == "_error" then
-        gotMessageType = "error"
-    elseif messageType == "_output" then
-        gotMessageType = "output"
-    else
-        gotMessageType = "output"
-
-        if messageType ~= nil then
-            table.insert(strings, messageType)
-        end
+        table.insert(strings, tostring(sourceScript))
     end
 
     if args and #args > 0 then
@@ -37,41 +40,54 @@ function debugHandler.debug(sourceScript: Script, messageType: string, ...: stri
         end
     end
 
-    if gotMessageType == "output" then
+    if (messageType ~= "error") and (tracebackMode ~= TracebackMode.Disabled) then
+        local tracebackInformation
+
+        if tracebackMode == TracebackMode.Enabled then
+            tracebackInformation = {
+                debug.traceback("", 2)
+            }
+        elseif tracebackMode == TracebackMode.EnabledOpen then
+            tracebackInformation = debug.traceback("", 2)
+        elseif tracebackMode == TracebackMode.Code then
+            tracebackInformation = " (Code: ".. strings[1] .. ")"
+            table.remove(strings, 1) -- remove the code from the outputted message
+        end
+
+        table.insert(args, tracebackInformation)
+    end
+
+    if messageType == MessageType.Output then
         print(unpack(strings))
-    elseif gotMessageType == "warn" then
+    elseif messageType == MessageType.Warning then
         warn(unpack(strings))
-    elseif gotMessageType == "error" then
-        error(table.concat(strings, " "))
+    elseif messageType == MessageType.Error then
+        error(table.concat(strings, " ")) -- does not require a traceback method, automatically creates a traceback
     end
 end
 
-function debugHandler.GetPrint(sourceScript: Script)
-    return function(...: string)
-        return debugHandler.debug(sourceScript, "_output", ...)
+function debugHandler.GetPrint(sourceScript: Script, TopLevelTracebackMode: TracebackMode?)
+    return function(overrideTracebackMode: TracebackMode?, ...: string)
+        return debugHandler.debug(sourceScript, MessageType.Output, overrideTracebackMode or TopLevelTracebackMode, ...)
     end
 end
 
-function debugHandler.GetWarn(sourceScript: Script)
-    return function(...: string)
-        return debugHandler.debug(sourceScript, "_warn", ...)
+function debugHandler.GetWarn(sourceScript: Script, TopLevelTracebackMode: TracebackMode?)
+    return function(overrideTracebackMode: TracebackMode?, ...: string)
+        return debugHandler.debug(sourceScript, MessageType.Warning, overrideTracebackMode or TopLevelTracebackMode, ...)
     end
 end
 
-function debugHandler.GetError(sourceScript: Script)
-    return function(...: string)
-        return debugHandler.debug(sourceScript, "_error", ...)
+function debugHandler.GetError(sourceScript: Script, TopLevelTracebackMode: TracebackMode?)
+    return function(overrideTracebackMode: TracebackMode?, ...: string)
+        return debugHandler.debug(sourceScript, MessageType.Error, overrideTracebackMode or TopLevelTracebackMode, ...)
     end
 end
 
-function debugHandler.GetDebugs(sourceScript: Script)
-    return function (...: string)
-        return debugHandler.debug(sourceScript, "_output", ...)
-    end, function(...: string)
-        return debugHandler.debug(sourceScript, "_warn", ...)
-    end, function(...: string)
-        return debugHandler.debug(sourceScript, "_error", ...)
-    end
+function debugHandler.GetDebugs(sourceScript: Script, TopLevelTracebackMode: TracebackMode?)
+    return debugHandler.GetPrint(sourceScript, TopLevelTracebackMode), 
+        debugHandler.GetWarn(sourceScript, TopLevelTracebackMode), 
+        debugHandler.GetError(sourceScript, TopLevelTracebackMode)
 end
 
 
